17a819dd531e02b5cfc725115603a478c06abcf6
Manan <manan1206@gmail.com>
2020-08-19 01:09:58 +0530

    Classify_clothes route added

--- Flask_Backend/server.py
+++ Flask_Backend/server.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+import os
+import sys
+
+sys.path.append('D:\Projects\Image_Processing_web_app\Python_Scripts\clothes-classifier')
+from clothes_model import classify_clothes
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/classifyclothes')
+def classify_image():
+    image_name = request.args.get('image_name')
+    if not image_name:
+        return 'Please Provide an image'
+    output = classify_clothes(image_name)
+    return output
+    # os.chdir('D:\Projects\Image_Processing_web_app\Python_scripts\clothes-classifier')
+    # os.system('python clothes_model.py {}', image_name)  
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
